4f71e282eb0ee31e3f6cd4576c729eb6352c3924
Flo <enrolf254@gmail.com>
2024-07-01 12:05:09 +0200

    vector

--- Projects/Vector graphics/Vector.js
+++ Projects/Vector graphics/Vector.js
@@ -1,3 +1,11 @@
 console.log("Test");
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext("2d");
+//x-Axis
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 0);
+ctx.stroke();
+//y-Axis
+ctx.moveTo(0, 0);
+ctx.lineTo(0, 200);
+ctx.stroke();
--- Projects/Vector graphics/Vector.ts
+++ Projects/Vector graphics/Vector.ts
@@ -3,8 +3,13 @@ console.log("Test");
 const canvas: HTMLCanvasElement = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
+//x-Axis
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 0);
+ctx.stroke();
 
-
-
-
+//y-Axis
+ctx.moveTo(0, 0);
+ctx.lineTo(0, 200);
+ctx.stroke();
 
