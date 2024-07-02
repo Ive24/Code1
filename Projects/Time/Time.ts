dca65f244563fe1bbba53c4973ee2ebd0720c15a
Flo <enrolf254@gmail.com>
2024-07-02 11:42:17 +0200

    Time2

--- Projects/Time/Time.ts
+++ Projects/Time/Time.ts
@@ -1,5 +1,16 @@
 function runLater () {
-    console.log ("run me now!")
+    console.log ("run me now!");
 }
 
 setTimeout(runLater, 1000); 
+
+let runs: number = 0;
+let intervalID: number = setInterval(runEverySecond, 40); 
+
+function runEverySecond () {
+    runs++;
+    console.log("I'm running every second. This is run number", runs);
+    if (runs >= 5)
+        clearInterval(intervalID); 
+}
+
