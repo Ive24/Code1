389915afc9e152d82136588abc4a041d3c4ca41f
Flo <enrolf254@gmail.com>
2024-07-04 13:07:11 +0200

    BI

---
+++ Projects/BasicInteraction/BI.js
@@ -0,0 +1,37 @@
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+canvas.addEventListener("click", function (event) {
+    var rect = canvas.getBoundingClientRect();
+    var scaleX = canvas.width / rect.width;
+    var scaleY = canvas.height / rect.height;
+    var PosX = (event.clientX - rect.left) * scaleX;
+    var PosY = (event.clientY - rect.top) * scaleY;
+    var shapeType = Math.random() < 0.5 ? "circle" : "square";
+    shapes.push({ x: PosX, y: PosY, type: shapeType });
+    //   drawShapes();
+});
+var shapes = [];
+var speed = 0.5;
+function drawShapes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (var _i = 0, shapes_1 = shapes; _i < shapes_1.length; _i++) {
+        var shape = shapes_1[_i];
+        shape.y += speed;
+    }
+    if (shapes. > canvas.height) {
+    }
+    shapes.forEach(function (shape) {
+        if (shape.type === "circle") {
+            ctx.beginPath();
+            ctx.arc(shape.x, shape.y, 20, 0, 2 * Math.PI);
+            ctx.fillStyle = "darkgreen";
+            ctx.fill();
+        }
+        else if (shape.type === "square") {
+            ctx.fillStyle = "lightblue";
+            ctx.fillRect(shape.x - 20, shape.y - 20, 40, 40);
+        }
+    });
+    requestAnimationFrame(drawShapes); // Animationsschleife für kontinuierliche Bewegung
+}
+drawShapes();
---
+++ Projects/BasicInteraction/BI.ts
@@ -0,0 +1,55 @@
+const canvas: HTMLCanvasElement = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+  canvas.addEventListener("click", (event) => {
+      let rect = canvas.getBoundingClientRect();
+      let scaleX = canvas.width / rect.width;
+      let scaleY = canvas.height / rect.height;
+
+      let PosX = (event.clientX - rect.left) * scaleX;
+      let PosY = (event.clientY - rect.top) * scaleY;
+
+      let shapeType = Math.random() < 0.5 ? "circle": "square";
+      shapes.push({ x: PosX, y: PosY, type: shapeType });
+
+    //   drawShapes();
+})
+
+
+interface Shape {
+    x: number,
+    y: number,
+    type: string,
+}
+
+let shapes: Shape [] = [];
+
+let speed: number = 0.5;
+
+
+function drawShapes() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    for (let shape of shapes) {
+        shape.y += speed;
+    }
+    if (shapes. > canvas.height) {
+
+    }
+
+    shapes.forEach((shape) => {
+
+        if (shape.type === "circle") {
+            ctx.beginPath();
+            ctx.arc(shape.x, shape.y, 20, 0, 2 * Math.PI);
+            ctx.fillStyle = "darkgreen";
+            ctx.fill();
+        } else if (shape.type === "square") {
+            ctx.fillStyle = "lightblue";
+            ctx.fillRect(shape.x - 20, shape.y - 20, 40, 40);
+        }
+    });
+
+    requestAnimationFrame(drawShapes); // Animationsschleife für kontinuierliche Bewegung
+}
+
+drawShapes();
